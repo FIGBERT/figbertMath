2ad58cd5e9fa5464f3aeea80856a1ee10810fb46
therealFIGBERT <figbertwelner@gmail.com>
2019-11-22 08:24:16 -0800

    Move App files into the components directory and merge with calculator.js

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Calculator } from "./components/calculator";
-import './App.css';
-
-export class App extends React.Component {
-  render() {
-    return <Calculator/>;
-  }
-}
