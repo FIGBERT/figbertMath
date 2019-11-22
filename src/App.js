25d6c1497f4c489108291e8d9ff4c62d5c89ca0a
therealFIGBERT <figbertwelner@gmail.com>
2019-11-21 21:49:13 -0800

    Add calculator output screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Calculator } from "./components/calculator";
 import './App.css';
 
 export class App extends React.Component {
   render() {
-    return <div>Hello world!</div>;
+    return <Calculator/>;
   }
 }
